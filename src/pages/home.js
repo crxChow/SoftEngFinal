a7dc49cb01203f571e5aad8521daae1caee2618d
Gabe Demanche <gldemanche@wpi.edu>
2022-12-04 23:15:04 -0500

    no wifi but updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h1>Welcome to our amazing platform</h1>
       <h2>report any issues to chris</h2>
-      <h2>its probably his fault anyways</h2>
+      <h2>or gabe, hes more likely to fix it</h2>
     </div>
   );
 };
